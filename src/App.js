a6ac425a1dfb1c009b662e06e1928f0267c3ede0
anonymous-lad2 <anonymouslad540@gmail.com>
2024-11-16 19:05:36 +0530

    Food delivery app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='h-screen flex flex-col justify-center items-center bg-green-600 font-bold'>
+      <h1 className='text-3xl'>Hello guyz</h1>
+      <h2 className='text-xl'>welcome to online food delivery app!!</h2>
+    </div>
+  );
+}
+
+export default App;
