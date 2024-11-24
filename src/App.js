8de6ba2a2916404e39465e49917021d19233a21d
anonymous-lad2 <anonymouslad540@gmail.com>
2024-11-24 13:21:39 +0530

    Adding Paths

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import Header from './components/Header';
-import { useState } from 'react';
-import Home from './pages/Home';
-import Main from './pages/Main';
-import Testimonial from './components/Testimonial';
-import Footer from './pages/Footer';
+import Header from "./components/Header";
+import { useState } from "react";
+import Home from "./pages/Home";
+import Main from "./pages/Main";
+import Testimonial from "./components/Testimonial";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./pages/Footer";
+import Error from "./pages/Error";
 
 function App() {
-
   const [darkTheme, setDarkTheme] = useState(false);
+
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div className={`scroll-w-0 ${darkTheme ? "dark" : ""}`}>
+          <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen px-8">
+            <Home darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+            <Main />
+            <Testimonial />
+            <Footer />
+          </div>
+        </div>
+      ),
+      children: [
+        {
+          path: "/about",
+          element: <div>ff</div>,
+        },
+        {
+          path: "/contact",
+          element: <Footer />,
+        },
+      ],
+      errorElement: (
+        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+          <Header darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+          <Error/>
+        </div>
+      ),
+    },
+  ]);
 
   return (
     <div className={`scroll-w-0 ${darkTheme ? "dark" : ""}`}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen px-8">
-        <Home darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
-        <Main/>
-        <Testimonial/>
-        <Footer/>
+        <Home darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Main />
+        <Testimonial />
+        <Footer />
       </div>
     </div>
   );
